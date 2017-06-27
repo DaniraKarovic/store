6ad5d92492eae3b96f55452f0273f062aa4ac7df
DaniraKarovic <karovic.danira@gmail.com>
2017-06-27 19:47:20 +0200

    added table and format to the page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,11 +2,12 @@ class HomeController < ApplicationController
   def index
     @title_message = 'Ruby on Rails for beginners 2017'
     @subtitle_message = 'Web shop demo application'
-
+    @product_count = Product.count
   end
 
   def products
-    @products = Product.all
+    @products = Product.all.order(price: :desc)
+    
   end
 
   def about
